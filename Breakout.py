17e950032ec211f5de33c0bc967df4067ca95547
Jed Rembold <jedediah.r@gmail.com>
2021-09-26 14:56:54 -0700

    Add explanatory comments to initial constants

--- Breakout.py
+++ Breakout.py
@@ -8,21 +8,21 @@ from pgl import GWindow, GOval, GRect, GState
 import random
 
 # Constants
-GWINDOW_WIDTH = 360
-GWINDOW_HEIGHT = 600
-N_ROWS = 10
-N_COLS = 10
-BRICK_ASPECT_RATIO = 4 / 1
-BRICK_TO_BALL_RATIO = 3 / 1  
-BRICK_TO_PADDLE_RATIO = 2 / 3
-BRICK_SEP = 2
-TOP_FRACTION = 0.1
-BOTTOM_FRACTION = 0.05
-N_BALLS = 3
-TIME_STEP = 10
-INITIAL_Y_VELOCITY = 3.0
-MIN_X_VELOCITY = 1.0
-MAX_X_VELOCITY = 3.0
+GWINDOW_WIDTH = 360                     # Width of the graphics window
+GWINDOW_HEIGHT = 600                    # Height of the graphics window
+N_ROWS = 10                             # Number of brick rows
+N_COLS = 10                             # Number of brick columns
+BRICK_ASPECT_RATIO = 4 / 1              # Width to height ratio of a brick
+BRICK_TO_BALL_RATIO = 3 / 1             # Ratio of brick width to ball size
+BRICK_TO_PADDLE_RATIO = 2 / 3           # Ratio of brick to paddle width
+BRICK_SEP = 2                           # Separation between bricks (in pixels)
+TOP_FRACTION = 0.1                      # Fraction of window above bricks
+BOTTOM_FRACTION = 0.05                  # Fraction of window below paddle
+N_BALLS = 3                             # Number of balls (lives) in a game
+TIME_STEP = 10                          # Time step in milliseconds
+INITIAL_Y_VELOCITY = 3.0                # Starting y velocity downwards
+MIN_X_VELOCITY = 1.0                    # Minimum random x velocity
+MAX_X_VELOCITY = 3.0                    # Maximum random x velocity
 
 # Derived Constants
 BRICK_WIDTH = (GWINDOW_WIDTH - (N_COLS + 1) * BRICK_SEP) / N_COLS
